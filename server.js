e6e1a453c384f6b4c06250145c170e2b478cc85c
Priyansu67 <25521478+Priyansu67@users.noreply.github.com>
2023-07-15 23:35:45 +0530

    ES6

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const connectDB = require("./connection/connection.js");
-const { Conversation } = require("./models/userConversationSchema.js");
+const  Conversation = require("./models/userConversationSchema.js");
 connectDB();
 
 const access_token = process.env.ACCESS_TOKEN;
